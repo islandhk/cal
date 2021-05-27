2a0e45f4ffd85062db4eb2fed9dc565718901b85
geenva <geenva@users.noreply.github.com>
2021-05-27 20:44:08 +0800

    ✨ Schemas

---
+++ src/models/user.ts
@@ -0,0 +1,10 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "../bot/types/Models";
+
+const UserSchema = new Schema<IUser>({
+  id: { type: String, required: true },
+  calendar: { type: String, required: true },
+});
+
+const User = mongoose.model<IUser>("Users", UserSchema);
+export default User;

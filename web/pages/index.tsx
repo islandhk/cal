bcd049e5065f9d56d989a332876801e530ee44fd
geenva <geenva@users.noreply.github.com>
2021-06-06 21:56:00 +0800

    🩹 Fix links

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,31 +12,33 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Start &rarr;</h2>
-            <p>Register your calendar.</p>
-          </a>
+          <Link href="/start">
+            <a className={styles.card}>
+              <h2>Start &rarr;</h2>
+              <p>Register your calendar.</p>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Delete &rarr;</h2>
-            <p>Delete your data with us.</p>
-          </a>
+          <Link href="/delete">
+            <a className={styles.card}>
+              <h2>Delete &rarr;</h2>
+              <p>Delete your data with us.</p>
+            </a>
+          </Link>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Invite &rarr;</h2>
-            <p>invite Cal to your server.</p>
-          </a>
+          <Link href="https://discord.com/oauth2/authorize?client_id=847450876918104094&scope=bot&permissions=2214980672">
+            <a className={styles.card}>
+              <h2>Invite &rarr;</h2>
+              <p>invite Cal to your server.</p>
+            </a>
+          </Link>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Server &rarr;</h2>
-            <p>Join our Discord server.</p>
-          </a>
+          <Link href="https://discord.gg/gStGqzGF7M">
+            <a className={styles.card}>
+              <h2>Server &rarr;</h2>
+              <p>Join our Discord server.</p>
+            </a>
+          </Link>
         </div>
       </main>
     </div>

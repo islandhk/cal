305b4e50e573ca9d257a44dcd1ecf5ae50c1a7c2
geenva <geenva@users.noreply.github.com>
2021-06-08 22:00:19 +0800

    2

--- web/pages/start/index.tsx
+++ web/pages/start/index.tsx
@@ -1,7 +1,43 @@
 import { useSession } from "next-auth/client";
+import styles from "../../styles/Start.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { connect } from "mongoose";
+import User from "../../models/user";
 
-export default function Start() {
+export default async function Start() {
+  const [URL, setURL] = useState(null);
+
   const [session, loading] = useSession();
 
-  return null;
+  const router = useRouter();
+
+  const redirect = () => {
+    router.push("/api/auth/signin");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setURL(e.target.value);
+  };
+
+  return (
+    <main className={styles.main}>
+      {session ? null : redirect()}
+      <h1>Welcome, {session.user.name}.</h1>
+
+      <p>What's your Gateway URL?</p>
+      <form>
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Your Gateway URL"
+        />
+        <input onClick={handleSubmit} type="submit" value="Submit" />
+      </form>
+    </main>
+  );
 }
